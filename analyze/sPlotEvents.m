1f09e10254b1bd87e86347aad9e5c0d9415c3c82
David Souto <ds572@le.ac.uk>
2021-04-17 11:01:48 +0100

    Delete analyze directory

--- analyze/sPlotEvents.m
+++
@@ -1,7 +0,0 @@
-function sPlotEvents(Ctrl, SCR, offset, scale)
-% SCR is some event
-% Ctrl is a fixed parameter (or controled variable) (refresh rate, etc.)
-hold on;
-for i=1:length(SCR)
-    plot(Ctrl.sdur*[SCR(i) SCR(i)], [(offset-scale) (offset+scale)], '-m');
-end
